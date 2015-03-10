606f8d79b042b40450de81486e9d00f9aad46967
Kasper Isager <kasperisager@gmail.com>
2015-03-10 23:02:00 +0100

    Update Gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp')
-  , $ = require('gulp-load-plugins')()
+var gulp = require("gulp")
+  , $ = require("gulp-load-plugins")()
+  , del = require("del")
 
-gulp.task('images', function () {
-  return gulp.src('img/*.sketch')
+gulp.task("img", function () {
+  return gulp.src("img/*.sketch")
     .pipe($.sketch({
-      export: 'slices'
+      export: "slices"
     , formats: [
-        'png'
-      , 'svg'
+        "png"
+      , "svg"
       ]
     , scales: [
-        '1.0'
-      , '2.0'
+        "1.0"
+      , "2.0"
       ]
     }))
     .pipe($.ignore.exclude(/@2x.svg$/))
@@ -23,12 +24,12 @@ gulp.task('images', function () {
       , { removeDesc: true }
       ]
     })))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest("dist"))
     .pipe($.size({
       showFiles: true
     }))
 })
 
-gulp.task('watch', function () {
-  gulp.watch('img/*.sketch', ['images'])
+gulp.task("watch", function () {
+  gulp.watch("img/*.sketch", ["img"])
 })
